387688f15f2928bd3f16d3f745cd2c8ae2a43297
leonar21w <leonardsd.698@gmail.com>
2024-11-05 20:47:05 -0800

    Initial Commit

---
+++ MyProfessor/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  MyProfessor
+//
+//  Created by Leonard on 11/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	var body: some View {
+		Text(")
+	}
+}
+#Preview {
+    ContentView()
+}
---
+++ MyProfessor/MyProfessorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProfessorApp.swift
+//  MyProfessor
+//
+//  Created by Leonard on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MyProfessorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
