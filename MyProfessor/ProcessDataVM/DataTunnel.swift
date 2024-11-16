cc1734cc89532c70d3780d4701d47ec5f6c379df
leonar21w <leonardsd.698@gmail.com>
2024-11-16 00:29:09 -0800

    here ya go

---
+++ MyProfessor/ProcessDataVM/DataTunnel.swift
@@ -0,0 +1,8 @@
+//
+//  DataTunnel.swift
+//  MyProfessor
+//
+//  Created by Leonard on 11/15/24.
+//
+
+import Foundation
---
+++ MyProfessor/ProcessDataVM/DigestData.swift
@@ -0,0 +1,8 @@
+//
+//  DigestData.swift
+//  MyProfessor
+//
+//  Created by Leonard on 11/15/24.
+//
+
+import Foundation
